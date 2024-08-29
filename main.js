bf5bb67e5c77aaf72d81b553ab611da477ab6b32
Nicolás Krukowski <nicokrukowski@gmail.com>
2024-08-29 00:51:06 -0300

    Correccion de bugs

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ let lista = []
 console.log("Usted tiene " + lista.length + " items en la lista")
 
 
-let confirmar = confirm("¿Quiere agregar algo a la lista?")
+let confirmar = true
 
+confirmar = confirm("¿Quiere agregar algo a la lista?")
 
-for (let index = false; index != confirmar;) {
-    items = prompt("¿Que quiere agregar a la lista?")
+while (confirmar == true) {
+    let items = prompt("¿Que quiere agregar a la lista?")
     lista.push(items)
-
+    
     console.log(lista)
     console.log("Usted tiene " + lista.length + " items en la lista")
+
+    confirmar = confirm("¿Quiere agregar algo a la lista?")
 }
